dba8eb9d235c9923eaeebbf7f74f9df36fb9344e
Dapprentice <96088624+Dapprentice@users.noreply.github.com>
2022-04-05 16:04:36 -0300

    is not over more changes soon

---
+++ src/helpers/library.js
@@ -0,0 +1,20 @@
+import SDC11227 from "../images/SDC11227.JPG";
+import tita from "../images/tit.png";
+import movieLogo from "../images/movieLogo.png";
+
+export const movies = [
+    {
+        Movie:"",
+        Thumbnail:SDC11227,
+        Scene :SDC11227,
+        Logo:movieLogo,
+        Name :"",
+        Description:"Let go",
+        Genre :"Romance",
+        Actors :"Winner",
+        Year:"2003",
+        Duration:"12years",
+
+
+    },
+]
---
+++ src/pages/Home.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import "./Home.css";
+import { Logo } from '../images/Netflix';
+import { ConnectButton, Icon, TabList, Tab, Button } from "web3uikit";
+import { movies } from '../helpers/library';
+
+const Home = () => {
+  return(
+   <>
+    <div className="logo">
+      <Logo />
+    </div>
+    <div className="connect">
+       <Icon fill="#ffffff" size={24} svg="bell" />
+       <ConnectButton />
+    </div>
+    <div className="topBanner">
+       <TabList defaultActiveKey={1} tabStyle="bar">
+         <Tab tabKey={1} tabName={"Movies"}>
+           
+           <div className="scene">
+             <img src={movies[0].Scene} className="sceneImg"></img>
+             <img className="sceneLogo" src={movies[0].Logo}></img>
+             <p className="sceneDesc">{movies[0].Description}</p>
+             <div className="playButton">
+             <Button
+                      icon="chevronRightX2" 
+                      text="Play"
+                      theme=""
+                       type=""
+             /><Button/>
+             </div>
+           </div>
+
+         </Tab>
+        <Tab tabKey={2} tabName={'Series'} isDisabled={true}></Tab>
+         <Tab tabKey={3} tabName={'MyList'}></Tab>
+       </TabList>
+     
+     </div>
+  </>
+);
+};
+
+export default Home;
